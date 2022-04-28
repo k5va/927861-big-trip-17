67296134d995e9a32f7ac298f573872779e2370d
k5va <k5va@yandex.ru>
2022-04-28 18:36:53 +0300

    Added AddEventButtonView

---
+++ src/view/add-event-button/template.js
@@ -0,0 +1,11 @@
+/**
+ * Creates add new button template
+ * @returns {String} template
+ */
+const template = () =>
+  `<button
+    class="trip-main__event-add-btn btn btn--big btn--yellow"
+    type="button">New event
+  </button>`;
+
+export {template};
