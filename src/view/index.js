f254f88b3c8c902c185f19c5aec0d6e79ded4881
k5va <k5va@yandex.ru>
2022-04-28 19:06:56 +0300

    Added EventListView

--- src/view/index.js
+++ src/view/index.js
@@ -1,3 +1,5 @@
 export {default as AddEventButtonView} from './add-event-button/add-event-button';
 export {default as FiltersView} from './filters/filters';
 export {default as SortView} from './sort/sort';
+export {default as EventView} from './event/event';
+export {default as EventListView} from './event-list/event-list';
