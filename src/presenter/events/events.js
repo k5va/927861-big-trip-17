79bfb8daa2ad80edc0acba41c0f61b9fa84c3a3f
k5va <k5va@yandex.ru>
2022-04-28 19:43:22 +0300

    Added NUMBER_OF_EVENTS const

--- src/presenter/events/events.js
+++ src/presenter/events/events.js
@@ -1,6 +1,8 @@
 import { render } from '../../render';
 import { EditEventView, EventListView, EventView, SortView } from '../../view';
 
+const NUMBER_OF_EVENTS = 3;
+
 export default class EventsPresenter {
   _sortView = new SortView();
   _eventListView = new EventListView();
@@ -15,7 +17,7 @@ export default class EventsPresenter {
     render(this._sortView, this._container);
     render(this._eventListView, this._container);
     render(new EditEventView(), this._eventListView.getElement());
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < NUMBER_OF_EVENTS; i++) {
       render(new EventView(), this._eventListView.getElement());
     }
   }
