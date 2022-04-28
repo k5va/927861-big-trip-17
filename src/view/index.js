16c43dbdca13c09598fa3d20bebc61d57fcd3fcc
k5va <k5va@yandex.ru>
2022-04-28 18:49:49 +0300

    Added SortView

--- src/view/index.js
+++ src/view/index.js
@@ -1,2 +1,3 @@
 export {default as AddEventButtonView} from './add-event-button/add-event-button';
 export {default as FiltersView} from './filters/filters';
+export {default as SortView} from './sort/sort';
---
+++ src/view/sort/sort.js
@@ -0,0 +1,13 @@
+import AbstractView from '../abstract-view/abstract-view';
+import { template } from './template';
+
+
+export default class SortView extends AbstractView {
+  /**
+   * Returns view's html template
+   * @returns {String} - view's template
+   */
+  getTemplate() {
+    return template;
+  }
+}
