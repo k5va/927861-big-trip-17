20e38af08f60367ace8788d813604d0ba218fb6b
k5va <k5va@yandex.ru>
2022-04-30 16:11:24 +0300

    Added Offer model

---
+++ src/model/offer/offer.js
@@ -0,0 +1,30 @@
+export default class Offer {
+
+  /**
+   * Creates new instance of offer
+   * @param {Object} data - raw data
+   */
+  constructor(data) {
+    this.id = String(data['id']);
+    this.title = data['title'];
+    this.price = +data['price'];
+  }
+
+  /**
+   * Creates new offer from raw data
+   * @param {Object} data - raw data
+   * @returns {Offer} - offer
+   */
+  static parse(data) {
+    return new Offer(data);
+  }
+
+  /**
+   * Creates offers array from raw data
+   * @param {Array<Object>} data - raw data
+   * @returns {Array<Offer>} - array of offers
+   */
+  static parseAll(data) {
+    return data.map(Offer.parse);
+  }
+}
