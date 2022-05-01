f833904e5283798b81ebdd7062e16495afa4d856
k5va <k5va@yandex.ru>
2022-05-01 16:58:00 +0300

    Added PointType to const

---
+++ src/const/point-type/point-type.js
@@ -0,0 +1,13 @@
+const PointType = {
+  TAXI: 'Taxi',
+  BUS: 'Bus',
+  TRAIN: 'Train',
+  SHIP: 'Ship',
+  DRIVE: 'Drive',
+  FLIGHT: 'Flight',
+  CHECK_IN: 'Check-in',
+  SIGHTSEEING: 'Sightseeing',
+  RESTAURANT: 'Restaurant',
+};
+
+export default PointType;
