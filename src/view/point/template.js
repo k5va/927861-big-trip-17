9853c30924b3f817f12d3a369c99123bceec3c22
k5va <k5va@yandex.ru>
2022-05-02 10:15:22 +0300

    Fixed point type icon in point view

--- src/view/point/template.js
+++ src/view/point/template.js
@@ -31,7 +31,7 @@ const template = (point, offers) => {
     <div class="event">
       <time class="event__date" datetime="${eventDateFull}">${eventDate}</time>
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/check-in.png"
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png"
           alt="Event type icon">
       </div>
       <h3 class="event__title">${type} ${destination.name}</h3>
